e6387ae0c3242d78b0f779f134dc9cf4d76ddbe1
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-12 21:40:34 -0600

    commit tree 14733a35ac2c05130140908244be536f20348ed0

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string) {
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let final = [];
+    let arrayOfWords = string.split(" ")
+    for (let i = 0; i < arrayOfWords.length; i++) {
+      if (i == 0) {
+        final.push(this.capitalize( arrayOfWords[i]))
+      } else {
+        if (except.includes(arrayOfWords[i])) {
+          final.push(arrayOfWords[i])
+        } else {
+          final.push(this.capitalize(arrayOfWords[i]))
+        }
+      }
+
+    }
+    return final.join(" ");
+  }
 }
